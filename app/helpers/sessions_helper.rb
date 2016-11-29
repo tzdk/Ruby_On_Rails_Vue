d47501cd66b429fa33c3ae0c23d541ea5b10308c
tzdk <yefeng_dk@163.com>
2016-11-29 17:53:38 +0800

    oauth

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,7 @@ module SessionsHelper
   # 登入指定的用户
   def log_in(user)
     session[:user_id] = user.id
+    puts session[:user_id]
   end
   
   # 在持久会话中记住用户
@@ -16,16 +17,7 @@ module SessionsHelper
   end
   # 返回当前登录的用户（如果有的话）
   def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(:remember, cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
+    @current_user ||= User.find_by(name: params[:userName])
   end
   
   # 如果用户已登录，返回 true，否则返回 false
@@ -42,6 +34,8 @@ module SessionsHelper
   
     # 退出当前用户
   def log_out
+    puts "aaaa"
+    puts session[:user_id]
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
